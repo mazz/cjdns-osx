1927f8ea4fb1de84712b29de0519c3008cdebe84
Michael Hanna <mykelkim@yahoo.com>
2015-01-04 21:47:50 -0500

    Add placeholder view controller

---
+++ cjdns-osx/MAZStatusItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  MAZStatusItemViewController.h
+//  cjdns-osx
+//
+//  Created by maz on 2015-01-04.
+//  Copyright (c) 2015 maz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MAZStatusItemViewController : NSViewController
+
+@end
---
+++ cjdns-osx/MAZStatusItemViewController.m
@@ -0,0 +1,22 @@
+//
+//  MAZStatusItemViewController.m
+//  cjdns-osx
+//
+//  Created by maz on 2015-01-04.
+//  Copyright (c) 2015 maz. All rights reserved.
+//
+
+#import "MAZStatusItemViewController.h"
+
+@interface MAZStatusItemViewController ()
+
+@end
+
+@implementation MAZStatusItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
