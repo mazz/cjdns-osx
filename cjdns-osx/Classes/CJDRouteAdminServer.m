059135343a93932a71748fa5b4054b5f834df1ba
Michael <michaelkhanna@gmail.com>
2016-02-15 19:18:39 -0500

    rename to cjdroute.conf

--- cjdns-osx/Classes/CJDRouteAdminServer.m
+++ cjdns-osx/Classes/CJDRouteAdminServer.m
@@ -32,10 +32,11 @@ static CJDRouteAdminServer* _sharedServer = nil;
     
     _binPath = executablesDirectory;
     _port = kCJDRoutAdminDefaultPort;
-
-    NSString* conf = [configurationDirectory stringByAppendingPathComponent:@"cjdroute.conf"];
-    if ([[NSFileManager defaultManager] fileExistsAtPath:conf]) {
-        NSLog(@"conf: %@", conf);
+    _confPath = [configurationDirectory stringByAppendingPathComponent:@"cjdroute.conf"];
+    NSLog(@"_confPath: %@", _confPath);
+    
+    if (![[NSFileManager defaultManager] fileExistsAtPath:_confPath]) {
+        return nil;
     }
     return self;
 }
@@ -49,6 +50,92 @@ static CJDRouteAdminServer* _sharedServer = nil;
 {
     return [NSBundle mainBundle].resourcePath;
 }
+
+#pragma mark - Asynchronous Server Control Methods
+
+- (void)startWithCompletionHandler:(CJDRouteAdminServerControlCompletionHandler)completionBlock
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSError *error = nil;
+        BOOL serverDidStart = [self startServerWithError:&error];
+        if (!serverDidStart) {
+            if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(NO, error); });
+            return;
+        }
+        
+//        PostgresDataDirectoryStatus dataDirStatus = [PostgresServer statusOfDataDirectory:_varPath error:&error];
+//        
+//        if (dataDirStatus==PostgresDataDirectoryEmpty) {
+//            BOOL serverDidInit = [self initDatabaseWithError:&error];
+//            if (!serverDidInit) {
+//                if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(NO, error); });
+//                return;
+//            }
+//            
+//            BOOL serverDidStart = [self startServerWithError:&error];
+//            if (!serverDidStart) {
+//                if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(NO, error); });
+//                return;
+//            }
+//            
+//            BOOL createdUser = [self createUserWithError:&error];
+//            if (!createdUser) {
+//                if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(NO, error); });
+//                return;
+//            }
+//            
+//            BOOL createdUserDatabase = [self createUserDatabaseWithError:&error];
+//            if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(createdUserDatabase, error); });
+//        }
+//        else if (dataDirStatus==PostgresDataDirectoryCompatible) {
+//            BOOL serverDidStart = [self startServerWithError:&error];
+//            if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(serverDidStart, error); });
+//        }
+//        else {
+//            if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(NO, error); });
+//        }
+        
+    });
+}
+
+-(BOOL)startServerWithError:(NSError**)error {
+    NSTask *controlTask = [[NSTask alloc] init];
+    controlTask.launchPath = [self.binPath stringByAppendingPathComponent:@"cjdroute"];
+//    controlTask.arguments = @[
+//                              @"<",
+//                              self.confPath
+//                              ];
+    
+    controlTask.standardInput = [NSFileHandle fileHandleForReadingAtPath:self.confPath];
+//                              /* control command          */ @"start",
+//                                                             /* data directory           */ @"-D", self.varPath,
+//                                                             /* wait for server to start */ @"-w",
+//                                                             /* server log file          */ @"-l", self.logfilePath,
+//                                                             /* allow overriding port    */ @"-o", [NSString stringWithFormat:@"-p %lu", self.port]
+//                                                             ];
+    controlTask.standardError = [[NSPipe alloc] init];
+    [controlTask launch];
+    NSString *controlTaskError = [[NSString alloc] initWithData:[[controlTask.standardError fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding];
+    [controlTask waitUntilExit];
+    
+    if (controlTask.terminationStatus != 0 && error) {
+        NSMutableDictionary *errorUserInfo = [[NSMutableDictionary alloc] init];
+        errorUserInfo[NSLocalizedDescriptionKey] = NSLocalizedString(@"Could not start cjdroute admin server.",nil);
+        errorUserInfo[NSLocalizedRecoverySuggestionErrorKey] = controlTaskError;
+//        errorUserInfo[NSLocalizedRecoveryOptionsErrorKey] = @[@"OK", @"Open Server Log"];
+//        errorUserInfo[NSRecoveryAttempterErrorKey] = [[RecoveryAttempter alloc] init];
+//        errorUserInfo[@"ServerLogRecoveryOptionIndex"] = @1;
+//        errorUserInfo[@"ServerLogPath"] = self.logfilePath;
+        *error = [NSError errorWithDomain:@"me.maz.cjdns-osx.cjdroute" code:controlTask.terminationStatus userInfo:errorUserInfo];
+    }
+    
+    if (controlTask.terminationStatus == 0) {
+        self.isRunning = YES;
+    }
+    
+    return controlTask.terminationStatus == 0;
+}
+
 
 @end
 NS_ASSUME_NONNULL_END
